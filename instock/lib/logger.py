cb9b4c3bdc927562b61b93d9367d8025644dd386
louyongjiu <921944921@qq.com>
2024-12-05 17:35:06 +0800

    优化日志处理

--- instock/lib/logger.py
+++ instock/lib/logger.py
@@ -1,26 +1,50 @@
 import time
-from datetime import datetime
 import logging
-from functools import wraps
 import inspect
+from datetime import datetime
+from functools import wraps
+from contextlib import ContextDecorator
+
+class Timer(ContextDecorator):
+    def __init__(self):
+        self.start_time = None
+        self.end_time = None
+    def __enter__(self):
+        self.start_time = time.perf_counter()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.end_time = time.perf_counter()
+
+    @property
+    def elapsed(self):
+        return self.end_time - self.start_time if self.end_time is not None else 0.0
+
 
 def log_execution_details(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        # 获取调用者的信息
-        caller_frame = inspect.stack()[1]
-        caller_filename = caller_frame.filename
-        caller_lineno = caller_frame.lineno
-        start_time = time.time()
-        # print(f"Starting {func.__name__} at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-        arg_str = ', '.join([repr(a) for a in args] + [f"{k}={v!r}" for k, v in kwargs.items()])
-        logging.info(f"{func.__name__}({arg_str}) from {caller_filename}:{caller_lineno} 开始: {datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}")
-   
-        result = func(*args, **kwargs)
+        arg_str = ', '.join(repr(a) for a in args) + ', ' + ', '.join(f"{k}={v!r}" for k, v in kwargs.items())
         
-        end_time = time.time()
-        execution_time = end_time - start_time
-        # print(f"Finished {func.__name__} at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (took {execution_time:.4f} seconds)")
-        logging.info(f"{func.__name__}({arg_str}) from {caller_filename}:{caller_lineno} 完成: {datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')} , 耗时: {execution_time:.4f} 秒")
-        return result
+        # 获取调用者信息
+        caller_frame = inspect.currentframe().f_back
+        caller_filename = caller_frame.f_code.co_filename
+        caller_lineno = caller_frame.f_lineno
+
+        with Timer() as timer:
+            start_time_str = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+            logging.info(f"开始执行 {func.__name__}({arg_str}) at {caller_filename}:{caller_lineno} 时间: {start_time_str}")
+            
+            try:
+                result = func(*args, **kwargs)
+            except Exception as e:
+                end_time_str = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+                logging.error(f"执行 {func.__name__}({arg_str}) 失败 at {caller_filename}:{caller_lineno} 时间: {end_time_str}, 耗时: {timer.elapsed:.4f} 秒", exc_info=True)
+                raise e
+            
+            end_time_str = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+            logging.info(f"完成执行 {func.__name__}({arg_str}) at {caller_filename}:{caller_lineno} 时间: {end_time_str}, 耗时: {timer.elapsed:.4f} 秒")
+            
+            return result
+
     return wrapper
